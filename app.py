c5a24282f471b24789d48ab5d674859ba3850766
Nikolay Chetverikov <nikolay@chetverikov.pro>
2017-11-23 00:05:56 +0300

    change server name

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ import sys
 # routes contains the HTTP handlers for our server and must be imported.
 import routes
 
+class SSLWSGIRefServer(bottle.ServerAdapter):
+    def run(self, handler):
+        from wsgiref.simple_server import make_server, WSGIRequestHandler
+        import ssl
+        if self.quiet:
+            class QuietHandler(WSGIRequestHandler):
+                def log_request(*args, **kw): pass
+            self.options['handler_class'] = QuietHandler
+        srv = make_server(self.host, self.port, handler, **self.options)
+        srv.socket = ssl.wrap_socket (srv.socket,
+        certfile='server.pem', server_side=True)
+        srv.serve_forever()
+
 if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
     # Debug mode will enable more verbose output in the console window.
     # It must be set at the beginning of the script.
@@ -20,20 +33,6 @@ def wsgi_app():
     return bottle.default_app()
 
 if __name__ == '__main__':
-    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-
-    @bottle.route('/static/<filepath:path>')
-    def server_static(filepath):
-        """Handler for static files, used with the development server.
-        When running under a production server such as IIS or Apache,
-        the server should be configured to serve the static files."""
-        return bottle.static_file(filepath, root=STATIC_ROOT)
-
-    # Starts a local test server.
-    bottle.run(server='wsgiref', host=HOST, port=PORT)
+    app = bottle.Bottle()
+    srv = SSLWSGIRefServer(host="pyikuratelebot.azurewebsites.net", port=80)
+    bottle.run(server=srv)
